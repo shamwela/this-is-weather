4041d4018edb0a22f8f9002236c7ece7b5c40fa8
Sha Mwe La <shamwela2002@gmail.com>
2020-07-10 22:12:45 +0630

    Toggle Celsius & Farenheit and add Icons

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener("load", () => {
   );
   let temperatureDegree = document.querySelector(".temperature-degree");
   let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  let temperatureSpan = document.querySelector(".temperature span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -20,14 +22,39 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then((data) => {
+          console.log(data);
           //Get Data from the API
-          const { temperature, summary } = data.currently;
+          const { temperature, summary, icon } = data.currently;
 
           //Set DOM elements from the API
           temperatureDegree.textContent = temperature;
           temperatureDescription.textContent = summary;
-          locationTimezone.textContent = data.timezone;
+          locationTimezone.textContent = data.timezone; //because this data is not in "currently"
+
+          //Set Icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          //Toggle Celsius & Farenheit
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+            } else {
+              temperatureSpan.textContent = "F";
+            }
+          });
         });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+
+    //e.g. partly-cloudy-night => PARTLY_CLOUDY_NIGHT
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+
+    // start animation!
+    skycons.play();
+
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
